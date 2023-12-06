c28f4d575f58f1be493ed1e88eff20e1f7c9e04c
Bash <>
2023-12-06 19:52:19 +1100

    An actual implementation of day 5 part 2.

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,11 +1,20 @@
-use std::ops::Range;
+use std::{ops::Range, time::Instant};
 
 const INPUT: &str = include_str!("input.txt");
 const INPUT_SAMPLE: &str = include_str!("input_sample.txt");
 
 fn main() {
+    let start = Instant::now();
     part_1();
+    let fin = Instant::now().duration_since(start);
+    println!("Part 1: {}ms", fin.as_secs_f64() * 1000.0);
+
+    println!("");
+
+    let start = Instant::now();
     part_2();
+    let fin = Instant::now().duration_since(start);
+    println!("Part 2: {}ms", fin.as_secs_f64() * 1000.0);
 }
 
 #[derive(Debug)]
@@ -64,10 +73,137 @@ fn parse_input(input: &str) -> Data {
     Data { seeds, maps }
 }
 
-fn find_min(data: &Data) -> i64 {
+fn part_1() {
+    let data = parse_input(INPUT);
+
+    let min = data
+        .seeds
+        .iter()
+        .map(|s| {
+            data.maps.iter().fold(*s, |s, maps| {
+                let offset = maps
+                    .iter()
+                    .find_map(|m| {
+                        if m.range.contains(&s) {
+                            Some(m.offset)
+                        } else {
+                            None
+                        }
+                    })
+                    .unwrap_or(0);
+                s + offset
+            })
+        })
+        .min()
+        .unwrap();
+
+    println!("Min location: {}", min);
+}
+
+fn part_2() {
+    let data = parse_input(INPUT);
+
+    let mut source_ranges: Vec<Range<i64>> = Vec::new();
+    let mut dest_ranges: Vec<Range<i64>> = Vec::new();
+
+    for i in (0..data.seeds.len()).step_by(2) {
+        source_ranges.push(data.seeds[i]..data.seeds[i] + data.seeds[i + 1]);
+    }
+
+    // Need to progress through each layer of maps
+    data.maps.iter().for_each(|maps| {
+        // Pop source ranges, process into dest ranges. If any source ranges get split up and only get partially
+        // pushed into a dest range, push them back onto the source ranges so another iteration can take care of them.
+        while let Some(source) = source_ranges.pop() {
+            let got_mapped = maps
+                .iter()
+                .find(|&map| {
+                    // Range has already been emptied - finish
+                    if source.is_empty() {
+                        return true;
+                    }
+
+                    // Source range entirely outside mapping range - ignore
+                    if source.start >= map.range.end || source.end <= map.range.start {
+                        return false;
+                    }
+
+                    // Source range entirely inside mapping range - map whole range
+                    if source.start >= map.range.start && source.end <= map.range.end {
+                        dest_ranges.push(source.start + map.offset..source.end + map.offset);
+                        return true;
+                    }
+
+                    // Source range contains entire mapping range -
+                    //   push starting range back to source ranges
+                    //   push ending range back to source ranges
+                    //   map middle section
+                    if source.start <= map.range.start && source.end >= map.range.end {
+                        source_ranges.push(source.start..map.range.start);
+                        source_ranges.push(map.range.end..source.end);
+                        dest_ranges.push(map.range.start + map.offset..map.range.end + map.offset);
+                        return true;
+                    }
+
+                    // Source overlaps mapping start only -
+                    //   push starting range back to source ranges
+                    //   map remaining section
+                    if source.start <= map.range.start && source.end <= map.range.end {
+                        source_ranges.push(source.start..map.range.start);
+                        dest_ranges.push(map.range.start + map.offset..source.end + map.offset);
+                        return true;
+                    }
+
+                    // Source overlaps mapping end only -
+                    //   map starting range
+                    //   push remaining section back to source_ranges
+                    if source.start >= map.range.start && source.end >= map.range.end {
+                        dest_ranges.push(source.start + map.offset..map.range.end + map.offset);
+                        source_ranges.push(map.range.end..source.end);
+                        return true;
+                    }
+
+                    panic!("Missed a case!");
+                })
+                .is_some();
+
+            if !got_mapped {
+                dest_ranges.push(source);
+            }
+        }
+
+        assert!(source_ranges.is_empty());
+        std::mem::swap(&mut source_ranges, &mut dest_ranges);
+    });
+
+    let min: i64 = source_ranges
+        .iter()
+        .min_by_key(|r| {
+            assert!(!r.is_empty());
+            r.start
+        })
+        .unwrap()
+        .start;
+
+    println!("Min location of ranges: {}", min);
+}
+
+/*
+/// Original part 2 solution which just parallelised and brute-forced all the possible seeds.
+/// Took about 16.6gb of ram and all 24 of my cores att 100% to get it in about 15 seconds.
+fn part_2() {
     use rayon::prelude::*;
 
-    data.seeds
+    let mut data = parse_input(INPUT);
+
+    let mut new_seeds = Vec::new();
+    for i in (0..data.seeds.len()).step_by(2) {
+        new_seeds.extend(data.seeds[i]..data.seeds[i] + data.seeds[i + 1]);
+    }
+    data.seeds = new_seeds;
+
+    let min = data
+        .seeds
         .par_iter()
         .map(|s| {
             data.maps.iter().fold(*s, |s, maps| {
@@ -85,27 +221,8 @@ fn find_min(data: &Data) -> i64 {
             })
         })
         .min()
-        .unwrap()
-}
-
-fn part_1() {
-    let data = parse_input(INPUT);
-
-    let min = find_min(&data);
-
-    println!("Min location: {}", min);
-}
-
-fn part_2() {
-    let mut data = parse_input(INPUT);
-
-    let mut new_seeds = Vec::new();
-    for i in (0..data.seeds.len()).step_by(2) {
-        new_seeds.extend(data.seeds[i]..data.seeds[i] + data.seeds[i + 1]);
-    }
-    data.seeds = new_seeds;
-
-    let min = find_min(&data);
+        .unwrap();
 
     println!("Min location of ranges: {}", min);
 }
+*/
